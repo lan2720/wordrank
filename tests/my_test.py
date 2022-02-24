73261aef3690fa531261627d1efbff318d1b208a
jarvixwang(王嘉楠) <jarvixwang@futunn.com>
2022-02-25 01:29:05 +0800

    update

--- tests/my_test.py
+++ tests/my_test.py
@@ -1,5 +1,9 @@
 import wordrank
+from wordrank.features.text_feature import TextFeature
+from wordrank.features.statistics_feature import StatisticsFeature
+from wordrank.features.language_feature import LanguageFeature
 
 q = "腾讯的Q3财报"#'哪里下载电视剧周恩来？'
-r = wordrank.rank(q)
-print(r)
+# r = wordrank.rank(q)
+# print(r)
+tf = TextFeature()
