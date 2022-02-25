e208a4fa70e1eb4c1d2799ed062f4c4c57e449f8
lan2720 <jarvixwang@futunn.com>
2022-02-25 12:02:42 +0800

    test

--- tests/my_test.py
+++ tests/my_test.py
@@ -4,6 +4,6 @@ from wordrank.features.statistics_feature import StatisticsFeature
 from wordrank.features.language_feature import LanguageFeature
 
 q = "腾讯的Q3财报"#'哪里下载电视剧周恩来？'
-# r = wordrank.rank(q)
-# print(r)
+r = wordrank.rank(q)
+print(r)
 tf = TextFeature()
